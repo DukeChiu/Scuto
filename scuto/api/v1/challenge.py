00f81d26ddf2c4532d8de0b64b6131ea84fa158e
Semesse <Semperia@outlook.com>
2020-01-22 15:20:07 +0800

    add challenge api

--- scuto/api/v1/challenge.py
+++ scuto/api/v1/challenge.py
@@ -9,5 +9,19 @@ load = 0
 @routes('/challenge/search')
 @jsonify()
 async def search(request):
-    return search_image(**(await request.json()))
+    term, page, page_size = request.query.get('q'), request.query.get('page'), request.query.get('page_size')
+    return await search_image(term, page, page_size)
 
+@routes('/challenge/list')
+@jsonify()
+async def list_challenges(request):
+    return Challenge.objects()
+
+@routes('/challenge/edit_challenge', methods=['POST'])
+@jsonify()
+async def edit_challenge(request):
+    challenge = Challenge.from_json(await request.json())
+    challenge.save()
+    return {
+        'status': 'success'
+    }
